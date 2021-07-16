3c91c5931b139a391ab60a0a0c93257c399be4d0
extraes <52384576+extraes@users.noreply.github.com>
2021-07-16 01:03:15 -0700

    Add files via upload

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,43 @@
+﻿using MelonLoader;
+using ModThatIsNotMod.BoneMenu;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Random_slowmo
+{
+    public static class BuildInfo
+    {
+        public const string Name = "RandomSlowmo"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "extraes, Elarelda"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class RandomSlowmo : MelonMod
+    {
+        private System.Random rand = new System.Random();
+        private bool isEnabled = false;
+        public override void OnApplicationStart()
+        {
+            var menu = MenuManager.CreateCategory("Random slow motion", Color.grey);
+            menu.CreateBoolElement("Random Slow Motion", Color.grey, false, new Action<bool>((bool b) => { isEnabled = b; }));
+            _ = MelonCoroutines.Start(ChangeTime());
+        }
+        private IEnumerator ChangeTime()
+        {
+            yield return new WaitForSeconds(rand.Next(6, 10));
+
+            var times = new float[] { 0.125f, 0.25f, 0.5f };
+            if (isEnabled)
+            {
+                Time.timeScale = times[rand.Next(0, times.Length)];
+                yield return new WaitForSeconds(3);
+                Time.timeScale = 1;
+            }
+
+            _ = MelonCoroutines.Start(ChangeTime());
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(Random_slowmo.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Random_slowmo.BuildInfo.Company)]
+[assembly: AssemblyProduct(Random_slowmo.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Random_slowmo.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Random_slowmo.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(Random_slowmo.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Random_slowmo.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(Random_slowmo.RandomSlowmo), Random_slowmo.BuildInfo.Name, Random_slowmo.BuildInfo.Version, Random_slowmo.BuildInfo.Author, Random_slowmo.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
